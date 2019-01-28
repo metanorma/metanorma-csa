604f7f24213b7cab33c01c636b64eb2102237702
Nick Nicholas <opoudjis@optusnet.com.au>
2019-01-28 01:00:41 +0000

    better handling of CR in XML; https://github.com/riboseinc/metanorma-standoc/issues/43

--- lib/asciidoctor/csand/converter.rb
+++ lib/asciidoctor/csand/converter.rb
@@ -84,7 +84,7 @@ module Asciidoctor
         result << noko { |ixml| front node, ixml }
         result << noko { |ixml| middle node, ixml }
         result << "</csand-standard>"
-        result = textcleanup(result.flatten * "\n")
+        result = textcleanup(result)
         ret1 = cleanup(Nokogiri::XML(result))
         validate(ret1)
         ret1.root.add_namespace(nil, CSAND_NAMESPACE)
