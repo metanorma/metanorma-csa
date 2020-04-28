7f71a6af7276d40ad14b655863dffd267947df17
Nick Nicholas <opoudjis@gmail.com>
2020-04-28 13:14:47 +1000

    XSL:FO

--- lib/isodoc/csa/pdf_convert.rb
+++ lib/isodoc/csa/pdf_convert.rb
@@ -1,62 +1,26 @@
 # frozen_string_literal: true
 
-require_relative 'base_convert'
 require 'isodoc'
 
 module IsoDoc
   module Csa
     # A {Converter} implementation that generates CSA output, and a document
     # schema encapsulation of the document for validation
-    class PdfConvert < IsoDoc::PdfConvert
+
+    class PdfConvert < IsoDoc::XslfoPdfConvert
       def initialize(options)
         @libdir = File.dirname(__FILE__)
         super
       end
 
-      def default_fonts(options)
-        is_hans = options[:script] == 'Hans'
-
-        {
-          bodyfont: is_hans ? '"SimSun",serif' : '"Source Sans Pro",sans-serif',
-          headerfont: (is_hans ? '"SimHei",sans-serif' 
-                               : '"Source Sans Pro",sans-serif'),
-          monospacefont: '"Space Mono",monospace'
-        }
+      def convert(filename, file = nil, debug = false)
+        file = File.read(filename, encoding: "utf-8") if file.nil?
+        docxml, outname_html, dir = convert_init(file, filename, debug)
+        FileUtils.rm_rf dir
+        ::Metanorma::Output::XslfoPdf.new.convert(
+          filename, outname_html + ".pdf",
+          File.join(@libdir, "csa.standard.xsl"))
       end
-
-      def default_file_locations(options)
-        {
-          htmlstylesheet: html_doc_path('htmlstyle.scss'),
-          htmlcoverpage: html_doc_path('html_csa_titlepage.html'),
-          htmlintropage: html_doc_path('html_csa_intro.html'),
-          scripts_pdf: html_doc_path('scripts.pdf.html')
-        }
-      end
-
-      def googlefonts()
-        <<~HEAD.freeze
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i|Space+Mono:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,500" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Overpass:100,300,300i,600,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,700,900" rel="stylesheet">
-        HEAD
-      end
-
-      def make_body(xml, docxml)
-        body_attr = { lang: 'EN-US', link: 'blue', vlink: '#954F72',
-                      'xml:lang': 'EN-US', class: 'container' }
-        xml.body **body_attr do |body|
-          make_body1(body, docxml)
-          make_body2(body, docxml)
-          make_body3(body, docxml)
-        end
-      end
-
-      def html_toc(docxml)
-        docxml
-      end
-
-      include BaseConvert
     end
   end
 end
