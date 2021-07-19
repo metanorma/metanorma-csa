f1695c66c296a0884de3950d38face2d86778813
Nick Nicholas <opoudjis@gmail.com>
2021-07-19 23:36:34 +1000

    version bump

--- spec/isodoc/csaconvert_spec.rb
+++ spec/isodoc/csaconvert_spec.rb
@@ -275,89 +275,82 @@ RSpec.describe IsoDoc::Csa do
        </bibliography>
        </csa-standard>
     INPUT
-       <csa-standard xmlns="http://riboseinc.com/isoxml" type="presentation">
-       <preface>
-       <foreword obligation="informative" displayorder="1">
-          <title>Foreword</title>
-          <p id="A">This is a preamble</p>
-        </foreword>
-         <introduction id="B" obligation="informative" displayorder="2">
-          <title>Introduction</title>
-          <clause id="C" inline-header="false" obligation="informative">
-          <title depth="2">Introduction Subsection</title>
-        </clause>
-        </introduction></preface><sections>
-        <clause id="D" obligation="normative" displayorder="6">
-          <title depth="1">4.<tab/>Scope</title>
-          <p id="E">Text</p>
-        </clause>
-     
-        <clause id="H" obligation="normative" displayorder="4">
-          <title depth="1">2.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
-          <terms id="I" obligation="normative">
-          <title depth="2">2.1.<tab/>Normal Terms</title>
-          <term id="J"><name>2.1.1.</name>
-          <preferred>Term2</preferred>
-        </term>
-        </terms>
-        <definitions id="K"><title>2.2.</title>
-          <dl>
-          <dt>Symbol</dt>
-          <dd>Definition</dd>
-          </dl>
-        </definitions>
-        </clause>
-        <definitions id="L" displayorder="5"><title>3.</title>
-          <dl>
-          <dt>Symbol</dt>
-          <dd>Definition</dd>
-          </dl>
-        </definitions>
-        <clause id="M" inline-header="false" obligation="normative" displayorder="7">
-          <title depth="1">5.<tab/>Clause 4</title>
-          <clause id="N" inline-header="false" obligation="normative">
-          <title depth="2">5.1.<tab/>Introduction</title>
-        </clause>
-        <clause id="O" inline-header="false" obligation="normative">
-          <title depth="2">5.2.<tab/>Clause 4.2</title>
-        </clause></clause>
-     
-        </sections><annex id="P" inline-header="false" obligation="normative" displayorder="8">
-          <title><strong>Appendix A</strong><br/>(normative)<br/><strong>Annex</strong></title>
-          <clause id="Q" inline-header="false" obligation="normative">
-          <title depth="2">A.1.<tab/>Annex A.1</title>
-          <clause id="Q1" inline-header="false" obligation="normative">
-          <title depth="3">A.1.1.<tab/>Annex A.1a</title>
-          </clause>
-        </clause>
-        </annex><bibliography><references id="R" obligation="informative" normative="true" displayorder="3">
-          <title depth="1">1.<tab/>Normative References</title>
-        </references><clause id="S" obligation="informative" displayorder="9">
-          <title depth="1">Bibliography</title>
-          <references id="T" obligation="informative" normative="false">
-          <title depth="2">Bibliography Subsection</title>
-        </references>
-        </clause>
-        </bibliography>
-        </csa-standard>
+      <csa-standard xmlns="http://riboseinc.com/isoxml" type="presentation">
+      <preface>
+      <foreword obligation="informative" displayorder="1">
+         <title>Foreword</title>
+         <p id="A">This is a preamble</p>
+       </foreword>
+        <introduction id="B" obligation="informative" displayorder="2">
+         <title>Introduction</title>
+         <clause id="C" inline-header="false" obligation="informative">
+         <title depth="2">Introduction Subsection</title>
+       </clause>
+       </introduction></preface><sections>
+       <clause id="D" obligation="normative" displayorder="6">
+         <title depth="1">4.<tab/>Scope</title>
+         <p id="E">Text</p>
+       </clause>
+       <clause id="H" obligation="normative" displayorder="4">
+         <title depth="1">2.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
+         <terms id="I" obligation="normative">
+         <title depth="2">2.1.<tab/>Normal Terms</title>
+         <term id="J"><name>2.1.1.</name>
+         <preferred>Term2</preferred>
+       </term>
+       </terms>
+       <definitions id="K"><title>2.2.</title>
+         <dl>
+         <dt>Symbol</dt>
+         <dd>Definition</dd>
+         </dl>
+       </definitions>
+       </clause>
+       <definitions id="L" displayorder="5"><title>3.</title>
+         <dl>
+         <dt>Symbol</dt>
+         <dd>Definition</dd>
+         </dl>
+       </definitions>
+       <clause id="M" inline-header="false" obligation="normative" displayorder="7">
+         <title depth="1">5.<tab/>Clause 4</title>
+         <clause id="N" inline-header="false" obligation="normative">
+         <title depth="2">5.1.<tab/>Introduction</title>
+       </clause>
+       <clause id="O" inline-header="false" obligation="normative">
+         <title depth="2">5.2.<tab/>Clause 4.2</title>
+       </clause></clause>
+       </sections><annex id="P" inline-header="false" obligation="normative" displayorder="8">
+         <title><strong>Appendix A</strong><br/>(normative)<br/><strong>Annex</strong></title>
+         <clause id="Q" inline-header="false" obligation="normative">
+         <title depth="2">A.1.<tab/>Annex A.1</title>
+         <clause id="Q1" inline-header="false" obligation="normative">
+         <title depth="3">A.1.1.<tab/>Annex A.1a</title>
+         </clause>
+       </clause>
+       </annex><bibliography><references id="R" obligation="informative" normative="true" displayorder="3">
+         <title depth="1">1.<tab/>Normative References</title>
+       </references><clause id="S" obligation="informative" displayorder="9">
+         <title depth="1">Bibliography</title>
+         <references id="T" obligation="informative" normative="false">
+         <title depth="2">Bibliography Subsection</title>
+       </references>
+       </clause>
+       </bibliography>
+       </csa-standard>
     OUTPUT
   end
 
   it "injects JS into blank html" do
     FileUtils.rm_f "test.html"
     input = <<~"INPUT"
-      = Document title
-      Author
-      :docfile: test.adoc
-      :novalid:
-    INPUT
-    html = Asciidoctor.convert(input, backend: :csa, header_footer: true)
-    expect(xmlpp(strip_guid(html))).to be_equivalent_to xmlpp(<<~"OUTPUT")
-      #{BLANK_HDR}
-      <sections/>
+      <csa-standard xmlns="https://open.ribose.com/standards/csa">
+      <preface><foreword>
+      <pre>ABC</pre>
+      </foreword></preface>
       </csa-standard>
-    OUTPUT
-
+    INPUT
+    IsoDoc::Csa::HtmlConvert.new({}).convert("test", input, false)
     html = File.read("test.html", encoding: "utf-8")
     expect(html).to match(/jquery\.min\.js/)
     expect(html).to match(/Lato/)
