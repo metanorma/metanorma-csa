221c92ed24f53ee03273a55197a759c8ab0e3fed
Nick Nicholas <opoudjis@gmail.com>
2021-08-30 21:40:07 +1000

    version bump

--- lib/asciidoctor/csa/converter.rb
+++ lib/asciidoctor/csa/converter.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-require 'asciidoctor'
-require 'metanorma-generic'
-require 'isodoc/csa/html_convert'
-require 'isodoc/csa/pdf_convert'
-require 'isodoc/csa/word_convert'
-require 'isodoc/csa/presentation_xml_convert'
-require 'metanorma/csa'
-require 'fileutils'
+require "asciidoctor"
+require "metanorma-generic"
+require "isodoc/csa/html_convert"
+require "isodoc/csa/pdf_convert"
+require "isodoc/csa/word_convert"
+require "isodoc/csa/presentation_xml_convert"
+require "metanorma/csa"
+require "fileutils"
 
 module Asciidoctor
   module Csa
     class Converter < ::Asciidoctor::Generic::Converter
-      XML_ROOT_TAG = "csa-standard".freeze
-      XML_NAMESPACE = "https://www.metanorma.org/ns/csa".freeze
+      XML_ROOT_TAG = "csa-standard"
+      XML_NAMESPACE = "https://www.metanorma.org/ns/csa"
 
       register_for "csa"
 
@@ -21,46 +21,47 @@ module Asciidoctor
         Metanorma::Csa.configuration
       end
 
-      def personal_role(node, c, suffix)
+      def personal_role(node, contrib, suffix)
         role = node.attr("role#{suffix}")&.downcase || "full author"
-        c.role **{ type: role == "editor" ? "editor" : "author" } do |r|
+        contrib.role **{ type: role == "editor" ? "editor" : "author" } do |r|
           r.description role.strip.gsub(/\s/, "-")
         end
       end
 
       def metadata_committee(node, xml)
         return unless node.attr("technical-committee")
+
         xml.editorialgroup do |a|
           a.committee node.attr("technical-committee"),
-            **attr_code(type: node.attr("technical-committee-type"))
+                      **attr_code(type: node.attr("technical-committee-type"))
           i = 2
-          while node.attr("technical-committee_#{i}") do
+          while node.attr("technical-committee_#{i}")
             a.committee node.attr("technical-committee_#{i}"),
-              **attr_code(type: node.attr("technical-committee-type_#{i}"))
+                        **attr_code(type: node.attr("technical-committee-type_#{i}"))
             i += 1
           end
         end
       end
 
-      def title_validate(root)
+      def title_validate(_root)
         nil
       end
 
       def outputs(node, ret)
-        File.open(@filename + ".xml", "w:UTF-8") { |f| f.write(ret) }
-          presentation_xml_converter(node).convert(@filename + ".xml")
-          html_converter(node).convert(@filename + ".presentation.xml", 
-                                       nil, false, "#{@filename}.html")
-          doc_converter(node).convert(@filename + ".presentation.xml", 
-                                      nil, false, "#{@filename}.doc")
-          pdf_converter(node)&.convert(@filename + ".presentation.xml", 
-                                       nil, false, "#{@filename}.pdf")
+        File.open("#{@filename}.xml", "w:UTF-8") { |f| f.write(ret) }
+        presentation_xml_converter(node).convert("#{@filename}.xml")
+        html_converter(node).convert("#{@filename}.presentation.xml",
+                                     nil, false, "#{@filename}.html")
+        doc_converter(node).convert("#{@filename}.presentation.xml",
+                                    nil, false, "#{@filename}.doc")
+        pdf_converter(node)&.convert("#{@filename}.presentation.xml",
+                                     nil, false, "#{@filename}.pdf")
       end
 
-      def sections_cleanup(x)
+      def sections_cleanup(xml)
         super
-        x.xpath("//*[@inline-header]").each do |h|
-          h.delete('inline-header')
+        xml.xpath("//*[@inline-header]").each do |h|
+          h.delete("inline-header")
         end
       end
 
@@ -74,13 +75,13 @@ module Asciidoctor
           r["type"] == "author" or next
           role = r.at("./description").text
           %w{full-author contributor staff reviewer}.include?(role) or
-        @log.add("Document Attributes", nil,
-                 "#{role} is not a recognised role")
+            @log.add("Document Attributes", nil,
+                     "#{role} is not a recognised role")
         end
       end
 
-      def style(n, t)
-        return
+      def style(_node, _text)
+        nil
       end
 
       def html_converter(node)
@@ -89,6 +90,7 @@ module Asciidoctor
 
       def pdf_converter(node)
         return if node.attr("no-pdf")
+
         IsoDoc::Csa::PdfConvert.new(html_extract_attributes(node))
       end
 
