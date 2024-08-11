f53876cb64580a5503fa3426fef15a75a46751e6
Nick Nicholas <opoudjis@gmail.com>
2024-08-11 22:31:42 +1000

    move pdf_stylesheet to configuration: https://github.com/metanorma/metanorma-generic/issues/77

--- lib/metanorma-csa.rb
+++ lib/metanorma-csa.rb
@@ -1,12 +1,12 @@
-require 'asciidoctor' unless defined? Asciidoctor::Converter
-require_relative 'metanorma/csa/converter'
-require_relative 'isodoc/csa/html_convert'
-require_relative 'isodoc/csa/pdf_convert'
-require_relative 'isodoc/csa/word_convert'
-require_relative 'metanorma/csa/version'
+require "asciidoctor" unless defined? Asciidoctor::Converter
+require_relative "metanorma/csa/converter"
+require_relative "isodoc/csa/html_convert"
+require_relative "isodoc/csa/pdf_convert"
+require_relative "isodoc/csa/word_convert"
+require_relative "metanorma/csa/version"
 require "metanorma"
 
 if defined? Metanorma::Registry
-  require_relative 'metanorma/csa'
+  require_relative "metanorma/csa"
   Metanorma::Registry.instance.register(Metanorma::Csa::Processor)
 end
