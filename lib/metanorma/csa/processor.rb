e291d0fe6d9b440569c9f00361d4bf3fb0e0a459
Nick Nicholas <opoudjis@gmail.com>
2025-04-06 12:52:19 +1000

    grammars

--- lib/metanorma/csa/processor.rb
+++ lib/metanorma/csa/processor.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-require 'metanorma/processor'
+require "metanorma/processor"
 
 module Metanorma
   module Csa
     class Processor < Metanorma::Processor
-
       def initialize
         @short = :csa
         @input_format = :asciidoc
@@ -14,9 +13,9 @@ module Metanorma
 
       def output_formats
         super.merge(
-          html: 'html',
-          doc: 'doc',
-          pdf: 'pdf'
+          html: "html",
+          doc: "doc",
+          pdf: "pdf",
         )
       end
 
@@ -24,17 +23,21 @@ module Metanorma
         "Metanorma::Csa #{Metanorma::Csa::VERSION}"
       end
 
-      def output(isodoc_node, inname, outname, format, options={})
+      def output(isodoc_node, inname, outname, format, options = {})
         options_preprocess(options)
         case format
         when :html
-          IsoDoc::Csa::HtmlConvert.new(options).convert(inname, isodoc_node, nil, outname)
+          IsoDoc::Csa::HtmlConvert.new(options)
+            .convert(inname, isodoc_node, nil, outname)
         when :doc
-          IsoDoc::Csa::WordConvert.new(options).convert(inname, isodoc_node, nil, outname)
+          IsoDoc::Csa::WordConvert.new(options)
+            .convert(inname, isodoc_node, nil, outname)
         when :pdf
-          IsoDoc::Csa::PdfConvert.new(options).convert(inname, isodoc_node, nil, outname)
+          IsoDoc::Csa::PdfConvert.new(options)
+            .convert(inname, isodoc_node, nil, outname)
         when :presentation
-          IsoDoc::Csa::PresentationXMLConvert.new(options).convert(inname, isodoc_node, nil, outname)
+          IsoDoc::Csa::PresentationXMLConvert.new(options)
+            .convert(inname, isodoc_node, nil, outname)
         else
           super
         end
